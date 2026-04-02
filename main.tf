767e58d40e085b125162fc7b1eaf44a0ecd59a23
Artian-Hengst <artian.ramiz@gmail.com>
2026-04-02 14:21:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_s3_bucket" "frontend" {
+  bucket = "ismaj-einkaufsliste-bucket-12345"
+}
+
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
